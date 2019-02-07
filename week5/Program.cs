d6459c0bf32ba3ce7b3296d727df695251b7a958
visualstudio <visualstudio@230-PC27>
2019-02-07 14:07:26 -0500

    2:07 update

--- week5/Program.cs
+++ week5/Program.cs
@@ -10,6 +10,30 @@ namespace week5
     {
         static void Main(string[] args)
         {
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
+            Console.ReadLine();
+        }
+    }
+    class TestQuestion2
+    {
+        public int myFavouiteVariable = 0;
+        public void PlayingWithForLoops()
+        {
+            // write a for loop to add 10 numbers
+            for (;MyMethod() ; )
+            {
+                if (myFavouiteVariable > 10)
+                {
+                    break;
+                }
+                Console.WriteLine("i am in the for loop");
+            }
+        }
+        public bool MyMethod()
+        {
+            myFavouiteVariable++;
+            return true;
         }
     }
 }
